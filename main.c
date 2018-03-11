e9767787cef8b9b1ac5448507cc8b816944a31d4
BuildTools <tug64068@temple.edu>
2018-03-10 20:21:35 -0500

    parse_line + tokenize + token_vector data strct

--- main.c
+++ main.c
@@ -22,11 +22,19 @@ enum builtin_cmds {
     QUIT
 };
 
+typedef struct token_vector {
+    size_t length;
+    char** arr;
+} token_vector;
+
 void update_directory_vars();
 void input_loop();
 void parse_line(char*);
+token_vector tokenize(char*, char*);
 
 int main(int argc, char* argv[]) {
+    update_directory_vars();
+
     if (argc == 2) {
         FILE* fp;
         char* line = NULL;
@@ -36,18 +44,18 @@ int main(int argc, char* argv[]) {
             exit(1);
         }
         while ((len = getline(&line, &BUFFER_LENGTH, fp)) != -1) {
-            line[strcspn(line, "\n")] = 0; //trim trailing newline if it exists
+            line[strcspn(line, "\r\n")] = 0; //trim trailing newline if it exists
             if (DEBUG) {
                 printf("read line from file: %s\n", line);
             }
-            parse_line(line);
+            if (strlen(line) > 0) {
+                parse_line(line);
+            }
         }
-        fclose(fp);
         if (line)
             free(line);
+        fclose(fp);
     }
-
-    update_directory_vars();
 
     if(DEBUG) {
         printf("Initial vars:\n");
@@ -60,13 +68,52 @@ int main(int argc, char* argv[]) {
 }
 
 void input_loop() {
-
+    while (1) {
+        printf("%s/%s> ", CURRENT_PATH, SHELL_NAME);
+        char* line = NULL;
+        getline(&line, &BUFFER_LENGTH, stdin);
+        line[strcspn(line, "\r\n")] = 0; //trim trailing newline if it exists
+        if (strlen(line) > 0) {
+            parse_line(line);
+        }
+        if (line)
+            free(line);
+    }
 }
 
 void parse_line(char* line) {
     if (DEBUG) {
-        printf("parsing line: %s\n", line);
+        printf("parsing line: !%s!\n", line);
     }
+    token_vector tokens = tokenize(line, " ");
+    
+    printf("tokens:\n");
+    int i;
+    for (i = 0; i < tokens.length; i++) {
+        printf("%s\n", tokens.arr[i]);
+    }
+
+    if (tokens.arr != NULL)
+        free(tokens.arr);
+}
+
+token_vector tokenize(char* line, char* token) {
+    token_vector v;
+    v.length = 1;
+    v.arr = (char**) malloc(sizeof(char*) * v.length);
+    char* p = strtok(line, token);
+    int i = 0;
+    while (p != NULL) {
+        if (i == v.length) {
+            if (realloc(v.arr, sizeof(char*) * ++v.length) == NULL) {
+                printf("could not realloc mem during tokenize\n");
+                exit(1);
+            }
+        }
+        v.arr[i++] = p;
+        p = strtok(NULL, token);
+    }
+    return v;
 }
 
 void update_directory_vars() {
